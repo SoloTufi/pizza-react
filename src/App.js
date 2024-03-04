c2eb2da9eb9c847ba01badc3e58348d4e19939a1
SoloTufi <mat_990011@mail.ru>
2024-03-05 00:49:58 +0700

    Transfer of props to cards
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/App.js
+++ src/App.js
@@ -18,18 +18,8 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
-            <PizzaBlock />
+            <PizzaBlock title="Мексиканская" price={500} />
+            <PizzaBlock title="Hello" price={350} />
           </div>
         </div>
       </div>
