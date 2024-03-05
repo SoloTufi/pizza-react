d2e94e1a7456360dab5a0d65ee909c6cfc74ffcc
SoloTufi <mat_990011@mail.ru>
2024-03-05 15:21:04 +0700

    Changing the import
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
 
 import './scss/App.scss';
 
