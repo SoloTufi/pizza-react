d56fd0f7629d3634e1e48a3c93652c212fe812bb
SoloTufi <mat_990011@mail.ru>
2024-03-05 19:09:47 +0700

    Added route for cart
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 
 import Header from './components/Header';
+
+import Cart from './pages/Cart'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 
@@ -12,12 +14,11 @@ function App() {
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   );
