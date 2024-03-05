b15a8f28fdf26ca03e501089c1fa95b24af3b7ce
SoloTufi <mat_990011@mail.ru>
2024-03-05 22:52:22 +0700

    Create component search input
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 
 import LogoSVG from "../assets/img/pizza-logo.svg";
 
+import Search from "./Search/Search";
+
 function Header() {
   return (
     <div className="header">
@@ -15,6 +17,7 @@ function Header() {
             </div>
           </div>
         </Link>
+        <Search />
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
             <span>520 ₽</span>
