046cf8f3a59b1da39a5c26667f07ece1a63a4876
SoloTufi <mat_990011@mail.ru>
2024-03-09 02:22:32 +0700

    Typing for Cart
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -8,10 +8,13 @@ import CartItem from "../components/CartItem";
 
 import CartEmpty from "../components/CartEmpty";
 
-const Cart = () => {
+const Cart: React.FC = () => {
   const dispatch = useDispatch();
   const { totalPrice, items } = useSelector(selectCart);
-  const totalCount = items.reduce((sum, item) => sum + item.count, 0);
+  const totalCount = items.reduce(
+    (sum: number, item: any) => sum + item.count,
+    0
+  );
 
   const onClickClear = () => {
     if (window.confirm("Empty the trash?")) {
@@ -101,7 +104,7 @@ const Cart = () => {
           </div>
         </div>
         <div className="content__items">
-          {items.map((item) => (
+          {items.map((item: any) => (
             <CartItem key={item.id} image={item.imageUrl} {...item} />
           ))}
         </div>
