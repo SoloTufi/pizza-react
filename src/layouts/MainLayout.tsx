c990ba1feeb1ab62d37518f6a6c8c97c64a1e9fb
SoloTufi <mat_990011@mail.ru>
2024-03-09 02:22:49 +0700

    Typing for MainLayout
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 
-const MainLayout = () => {
+const MainLayout: React.FC = () => {
   return (
     <div className="wrapper">
       <Header />
