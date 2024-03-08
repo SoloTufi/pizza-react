c45f5dd78abd96bf14670f7ee811932df0d61345
SoloTufi <mat_990011@mail.ru>
2024-03-09 02:21:58 +0700

    Typing for NotFound
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import NotFoundInfo from "../components/NotFoundInfo/index";
-
-const NotFound = () => {
-  return <NotFoundInfo />;
-};
-
-export default NotFound;
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import NotFoundInfo from "../components/NotFoundInfo/NotFoundInfo";
+
+const NotFound: React.FC = () => <NotFoundInfo />;
+
+export default NotFound;
