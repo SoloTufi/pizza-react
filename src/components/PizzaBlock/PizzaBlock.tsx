861508eca5046c85100addbd1502cdec15e1319a
SoloTufi <mat_990011@mail.ru>
2024-03-09 21:56:40 +0700

    Typing & add link for PizzaBlock
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/components/PizzaBlock/PizzaBlock.tsx
+++ src/components/PizzaBlock/PizzaBlock.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 import { useDispatch, useSelector } from "react-redux";
-import { addItem, selectCartItemById } from "../../redux/slices/cartSlice";
+import {
+  CartItem,
+  addItem,
+  selectCartItemById,
+} from "../../redux/slices/cartSlice";
+import { Link } from "react-router-dom";
 
 const typeNames = ["тонкое", "традиционное"];
 
@@ -31,13 +36,14 @@ const PizzaBlock: React.FC<PizzaBlockProps> = ({
   const addedCount = cartItem ? cartItem.count : 0;
 
   const onClickAdd = () => {
-    const item = {
+    const item: CartItem = {
       id,
       title,
       price,
       image,
       type: typeNames[activeType],
       size: sizes[activeSize],
+      count: 0,
     };
     dispatch(addItem(item));
   };
@@ -45,8 +51,10 @@ const PizzaBlock: React.FC<PizzaBlockProps> = ({
   return (
     <div className="pizza-block-wrapper">
       <div className="pizza-block">
-        <img className="pizza-block__image" src={image} alt="Pizza" />
-        <h4 className="pizza-block__title">{title}</h4>
+        <Link key={id} to={`pizza/${id}`}>
+          <img className="pizza-block__image" src={image} alt="Pizza" />
+          <h4 className="pizza-block__title">{title}</h4>
+        </Link>
         <div className="pizza-block__selector">
           <ul>
             {types.map((type, id) => (
