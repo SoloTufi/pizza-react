15f62ae4fa044b1410f6933241bf00bc69a9574f
SoloTufi <mat_990011@mail.ru>
2024-03-09 21:53:51 +0700

    Typing for pizzasSlice
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

---
+++ src/redux/slices/pizzasSlice.ts
@@ -0,0 +1,85 @@
+import axios from "axios";
+
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+import { Sort } from "./filterSlice";
+
+type Pizza = {
+  id: string;
+  image: string;
+  title: string;
+  price: number;
+  sizes: number[];
+  types: number[];
+  rating: number;
+};
+
+export enum Status {
+  LOADING = "loading",
+  SUCCESS = "success",
+  ERROR = "error",
+}
+
+interface PizzaSliceState {
+  items: Pizza[];
+  status: Status;
+}
+
+const initialState: PizzaSliceState = {
+  items: [],
+  status: Status.LOADING, // loading | success | error
+};
+
+export type SearchPizzaParams = {
+  sortBy: string;
+  order: string;
+  category: string;
+  search: string;
+  currentPage: string;
+};
+
+export const fetchPizzas = createAsyncThunk<Pizza[], SearchPizzaParams>(
+  "pizza/fetchPizzasStatus",
+  async (params) => {
+    const { sortBy, order, category, search, currentPage } = params;
+    const { data } = await axios.get(
+      `https://65e74e9e53d564627a8e7a59.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}&${search}`
+    );
+
+    return data;
+  }
+);
+
+const pizzaSlice = createSlice({
+  name: "pizza",
+  initialState,
+  reducers: {
+    setItems(state, action: PayloadAction<Pizza[]>) {
+      state.items = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPizzas.pending, (state) => {
+        state.status = Status.LOADING;
+        state.items = [];
+      })
+      .addCase(
+        fetchPizzas.fulfilled,
+        (state, action: PayloadAction<Pizza[]>) => {
+          state.items = action.payload;
+          state.status = Status.SUCCESS;
+        }
+      )
+      .addCase(fetchPizzas.rejected, (state) => {
+        state.status = Status.ERROR;
+        state.items = [];
+      });
+  },
+});
+
+export const selectPizzaData = (state: RootState) => state.pizza;
+
+export const { setItems } = pizzaSlice.actions;
+
+export default pizzaSlice.reducer;
