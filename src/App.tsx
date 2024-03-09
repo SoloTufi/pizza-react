daa140d5bc064275bcd9af5445907624b47d91c1
SoloTufi <mat_990011@mail.ru>
2024-03-10 02:06:43 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+import React from "react";
+
 import { Routes, Route } from "react-router-dom";
 
-import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import FullPizza from "./pages/FullPizza";
@@ -8,12 +9,21 @@ import MainLayout from "./layouts/MainLayout";
 
 import "./scss/App.scss";
 
+const Cart = React.lazy(() => import("./pages/Cart"));
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route
+          path="/cart"
+          element={
+            <React.Suspense fallback={<div>Идёт загрузка корзины...</div>}>
+              <Cart />
+            </React.Suspense>
+          }
+        />
         <Route path="/pizza/:id" element={<FullPizza />} />
         <Route path="*" element={<NotFound />} />
       </Route>
